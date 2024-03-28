9a88a2035f405d993b0bf58d55fe4c6af260718c
Federico González <federico1818@gmail.com>
2024-03-28 19:12:44 -0300

    Add Roboto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'todoapp';
+    public tasks: WritableSignal<string[]> = signal([
+        'Install Angular Cli',
+        'Create project',
+        'Create component'
+    ])
 }
