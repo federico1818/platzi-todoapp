c0158933e6bbeaed3c42680408ac67625d7c640f
Federico González <federico1818@gmail.com>
2024-03-29 01:46:59 -0300

    add task interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, WritableSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './models/task';
 
 @Component({
     selector: 'app-root',
@@ -11,9 +12,18 @@ import { RouterOutlet } from '@angular/router';
 })
 
 export class AppComponent {
-    public tasks: WritableSignal<string[]> = signal([
-        'Install Angular Cli',
-        'Create project',
-        'Create component'
+    public tasks: WritableSignal<Task[]> = signal([
+        {
+            title: 'Install Angular Cli',
+            completed: false
+        },
+        {
+            title: 'Create project',
+            completed: false
+        },
+        {
+            title: 'Create component',
+            completed: false
+        }
     ])
 }
