f81a3d59d664d3040a94638cc763bfcd876db059
Federico González <federico1818@gmail.com>
2024-06-20 04:25:46 -0300

    add reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { Component, WritableSignal, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Task } from './models/task';
+import { CommonModule } from '@angular/common'
+import { Component, WritableSignal, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Task } from './models/task'
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet],
+    imports: [
+        CommonModule, 
+        RouterOutlet,
+        ReactiveFormsModule
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
+    public form: FormGroup = this.fb.group({
+        task: ['', Validators.required]
+    })
+    
+    constructor(
+        private fb: FormBuilder
+    ) {}
+
     public tasks: WritableSignal<Task[]> = signal([
         {
             title: 'Install Angular Cli',
@@ -26,6 +39,13 @@ export class AppComponent {
             completed: false
         }
     ])
+
+    public onSubmit() {
+        if(this.form.valid) {
+            this.addTask(this.form.value.task)
+            this.form.reset()
+        }
+    }
 
     public deleteTask(index: number): void {
         this.tasks.update((tasks: Task[]) => {
